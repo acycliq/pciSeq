b63cb40ceeb5a831c82b8ca5578ae64575b6ab88
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-05-21 20:40:53 +0100

    cell Layer looks ok (maybe the background image isnt aligned? not sure...)

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -2,13 +2,13 @@ function config() {
     var ini = [
         {
             name: 'default',
-            roi: {"x0": 0, "x1": 40000, "y0": 0, "y1": 46000},
-            imageSize: [227951, 262144],
-            cellBoundaries: './cellBoundaries.json',
-            cellData: './cellData.json',
+            roi: {"x0": 0, "x1": 20268, "y0": 0, "y1": 27352}, // Now I am confused!! (ah ok, see note below)
+            imageSize: [194250, 262144],
+            cellBoundaries: './cell_coords.json',
+            cellData: '../demo_data/MOUSE_FULL_CORONAL/cached_results/cellData.json',
             topo: './cellBoundaries.topojson',
             // geneData: './dashboard/data/img/default/csv/Dapi_overlays.csv',
-            tiles: 'https://raw.githubusercontent.com/acycliq/viewer_datastore/master/demo_data/human/map_tiles/262144px/{z}/{y}/{x}.png'
+            tiles: './img/262144px/{z}/{y}/{x}.png'
         }, // 1
     ];
     var out = d3.map(ini, function (d) {
@@ -17,3 +17,8 @@ function config() {
     return out
 }
 
+//
+// ok here it is... map tiles and fov arent produced by the same image, that's why the backround image and the data plotted are misaligned (I think!).
+// You padded the original image to make the longer side a multiple of 2000 whereas you just scaled it up to the correct dimension
+// when you did the pyramid tiles
+//
