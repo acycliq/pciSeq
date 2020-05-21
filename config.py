f918fa5cc1bedd437591d3f707ace241cc25790a
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-05-21 20:50:19 +0100

    moved folder

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ MOUSE_FULL_CORONAL = {
     'max_iter': 100,
     'scRNAseq': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse', 'scRNA', 'scRNAseq.csv.gz'),
     'saFile': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'spots.csv'),  # Spot attributes, contains x,y coordinates for the spots and their gene names
-    'expanded_cells':  os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'expanded_cells', 'expanded_cells.csv'),
+    'expanded_cells':  os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'expanded_cells.csv'),
     'exclude_genes': [],
     'drop_nan': False,  # That is only a temp solution. I will be removing that when everything is set correctly
 }
