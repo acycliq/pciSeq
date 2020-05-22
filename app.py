708c2c7d85f704d03d4c4a0f1548744087d5997e
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-05-22 01:23:21 +0100

    minor edit

--- app.py
+++ app.py
@@ -20,31 +20,31 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     case = 'MOUSE_FULL_CORONAL'  # 'MOUSE' or 'HUMAN'
-    use_cache = False
+    use_cache = False  # <--- I think i will remove that...
 
     my_config = getattr(config, case)
-    cache_dir = os.path.join(config.ROOT_DIR, 'demo_data', case, 'cached_results')
+    out_dir = os.path.join(config.ROOT_DIR, 'data', 'cell_call_demo_data',  case, 'cell_type_output')
 
     # 1. run the cell calling algo
-    if use_cache:
+    if use_cache:  # I said it above, I am saying it here too: I think i will remove that...
         try:
-            cellData = pd.read_csv(os.path.join(cache_dir, 'cellData.csv'))
-            geneData = pd.read_csv(os.path.join(cache_dir, 'geneData.csv'))
-            logger.info('Cached data loaded from %s' % cache_dir)
+            cellData = pd.read_csv(os.path.join(out_dir, 'cellData.csv'))
+            geneData = pd.read_csv(os.path.join(out_dir, 'geneData.csv'))
+            logger.info('Cached data loaded from %s' % out_dir)
         except IOError:
             logger.info('Could not read cache')
             cellData, geneData = varBayes(my_config)
     else:
         cellData, geneData = varBayes(my_config)
 
-    if not os.path.exists(cache_dir):
-        os.makedirs(cache_dir)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-    cellData.to_json(os.path.join(cache_dir, 'cellData.json'), orient='records')
-    logger.info('Saved at %s' % (os.path.join(cache_dir, 'cellData.json')))
+    cellData.to_json(os.path.join(out_dir, 'cellData.json'), orient='records')
+    logger.info('Saved at %s' % (os.path.join(out_dir, 'cellData.json')))
 
-    geneData.to_json(os.path.join(cache_dir, 'geneData.json'), orient='records')
-    logger.info('Saved at %s' % (os.path.join(cache_dir, 'geneData.json')))
+    geneData.to_json(os.path.join(out_dir, 'geneData.json'), orient='records')
+    logger.info('Saved at %s' % (os.path.join(out_dir, 'geneData.json')))
 
 
     print('Done')
