68ed6f6f0a34f6e5882fca3760cddc6c10a41703
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-05-22 01:27:05 +0100

    I moved 'CELL_TYPED_GENES' and 'CELL_TYPED_CELLS' inside he PREPROCESSOR but i dont it makes sense. I should move it back, it just my life easier with some post processing tasks...

--- config.py
+++ config.py
@@ -11,8 +11,10 @@ PREPROCESSOR = {
     'fov_size': 2000,   # implies that each fov is square with side length 2000px
     'FOVS_ACROSS': 11,
     'FOVS_DOWN': 14,
-    # 'fovs_across': 11,  # This is not an input, it is calculated in '''split_image'''. I put it here for convenience
-    # 'fovs_down': 14,    # This is not an input, it is calculated in '''split_image'''. I put it here for convenience
+
+    # Output. Save here the results from the algorithm (I should move that somewhere else...)
+    'CELL_TYPED_GENES': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'cell_type_output', 'geneData.json'),
+    'CELL_TYPED_CELLS': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'cell_type_output', 'cellData.json'),
 }
 
 
@@ -35,6 +37,7 @@ MOUSE = {
 }
 
 MOUSE_FULL_CORONAL = {
+    # Hyperparameters
     'CellCallMaxIter': 100,
     'CellCallTolerance': 0.02,
     'Inefficiency': 0.2,
@@ -45,9 +48,11 @@ MOUSE_FULL_CORONAL = {
     'rGene': 20,
     'rSpot': 2,
     'max_iter': 100,
+
+    # Inputs to the cell calling algorithm
     'scRNAseq': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'cell_type_input', 'scRNAseq.csv.gz'),
     'saFile': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'cell_type_input', 'spots.csv'),  # Spot attributes, contains x,y coordinates for the spots and their gene names
     'expanded_cells':  os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'cell_type_input', 'expanded_cells.csv'),
-    'exclude_genes': [],
-    'drop_nan': False,  # That is only a temp solution. I will be removing that when everything is set correctly
+    'exclude_genes': [], # Write here the genes you want to exclude from the cell calling algorithm
+    'drop_nan': False,  # That is only a temp solution. I will be removing that when everything is set correctly, Well remove it now!!
 }
