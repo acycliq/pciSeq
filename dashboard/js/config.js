24d54a660bd50e03af322fbcafe6c86867540c6a
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-05-22 09:44:59 +0100

    new datastore name

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -6,7 +6,7 @@ function config() {
             imageSize: [194250, 262144],
             cellBoundaries: './dashboard/cell_coords.json',
             cellData: './data/cell_call_demo_data/mouse_full_coronal/cell_type_output/cellData.json',
-            tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal/master/{z}/{y}/{x}.png',
+            tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_datastore/master/{z}/{y}/{x}.png',
              // 'https://raw.githubusercontent.com/acycliq/full_coronal/{z}/{y}/{x}.png',
         // },
         }, // 1
