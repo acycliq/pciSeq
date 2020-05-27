f8a7760cdcdfbb7fae3148ff39b18c5fdc517f6f
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-05-27 22:10:27 +0100

    Added mouse events but it is really slow...

--- dashboard/js/stage_markers.js
+++ dashboard/js/stage_markers.js
@@ -42,15 +42,15 @@ function app(all_geneData, map) {
         return out
     }
 
-    function scaleRamp(z){
-        return z===0? 0.125:
-            z===1? 0.125:
-                z===2? 0.125:
-                    z===3? 0.125:
-                        z===4? 0.125:
-                            z===5? 0.125:
-                                z===6? 0.125:
-                                    z===7? 0.125: 1
+    function scaleRamp(z) {
+        return z === 0 ? 0.125 :
+            z === 1 ? 0.125 :
+                z === 2 ? 0.125 :
+                    z === 3 ? 0.125 :
+                        z === 4 ? 0.125 :
+                            z === 5 ? 0.125 :
+                                z === 6 ? 0.0625 :
+                                    z === 7 ? 0.03125 : 1
     }
 
 
@@ -100,6 +100,8 @@ function app(all_geneData, map) {
                         var p = dapiConfig.t.transform(L.point([marker.x, marker.y]));
                         var coords = project([p.y, p.x]);
                         var markerSprite = new PIXI.Sprite(textures[0]);
+                        markerSprite.x0 = coords.x;
+                        markerSprite.y0 = coords.y;
                         markerSprite.x = coords.x;
                         markerSprite.y = coords.y;
                         markerSprite.anchor.set(0.5, 0.5);
@@ -107,8 +109,72 @@ function app(all_geneData, map) {
                         markerSprites.push(markerSprite);
                         markerSprite.legend = marker.city || marker.label;
                     });
-
                 })
+                var quadTrees = {};
+                var start = +new Date();
+                for (var z = map.getMinZoom(); z <= map.getMaxZoom(); z++) {
+                    if (z < 9){
+                        quadTrees[z] = null  // I do not need the tree, I wont do mouse events when zoom < 7
+                    }
+                    else {
+                        var rInit = ((z <= 7) ? 10 : 24) / utils.getScale(z);
+                        quadTrees[z] = solveCollision(markerSprites, {r0: rInit, zoom: z});
+                    }
+                }
+                var end = +new Date();
+                var time = end - start;
+                console.log('total execution time = ' + time / 1000 + 'sec');
+
+                function findMarker(ll) {
+                    var layerPoint = project(ll);
+                    var quadTree = quadTrees[utils.getMap().getZoom()];
+                    var marker;
+                    var rMax = quadTree.rMax;
+                    var found = false;
+                    quadTree.visit(function (quad, x1, y1, x2, y2) {
+                        if (!quad.length) {
+                            var dx = quad.data.x - layerPoint.x;
+                            var dy = quad.data.y - layerPoint.y;
+                            var r = quad.data.scale.x * 16;
+                            if (dx * dx + dy * dy <= r * r) {
+                                marker = quad.data;
+                                found = true;
+                            }
+                        }
+                        return found || x1 > layerPoint.x + rMax || x2 + rMax < layerPoint.x || y1 > layerPoint.y + rMax || y2 + rMax < layerPoint.y;
+                    });
+                    return marker;
+                }
+
+                map.on('click', function (e) {
+                    var redraw = false;
+                    if (focus) {
+                        focus.texture = textures[focus.textureIndex];
+                        focus = null;
+                        L.DomUtil.addClass(legend, 'hide');
+                        legendContent.innerHTML = '';
+                        redraw = true;
+                    }
+                    var marker = findMarker(e.latlng);
+                    if (marker) {
+                        marker.texture = focusTextures[marker.textureIndex];
+                        focus = marker;
+                        legendContent.innerHTML = marker.legend;
+                        L.DomUtil.removeClass(legend, 'hide');
+                        redraw = true;
+                    }
+                    if (redraw) utils.getRenderer().render(container);
+                });
+
+                var self = this;
+                map.on('mousemove', L.Util.throttle(function (e) {
+                    var marker = findMarker(e.latlng);
+                    if (marker) {
+                        L.DomUtil.addClass(self._container, 'leaflet-interactive');
+                    } else {
+                        L.DomUtil.removeClass(self._container, 'leaflet-interactive');
+                    }
+                }, 32));
             }
 
             var start = null;
@@ -122,7 +188,7 @@ function app(all_geneData, map) {
                 if (lambda > 1) lambda = 1;
                 lambda = lambda * (0.4 + lambda * (2.2 + lambda * -1.6));
                 markerSprites.forEach(function (markerSprite) {
-                    var targetScale = zoom <=7? scaleRamp(zoom):  1 / (2*utils.getScale(event.zoom));
+                    var targetScale = zoom <= 7 ? scaleRamp(zoom) : 1 / (2 * utils.getScale(event.zoom));
                     markerSprite.scale.set(targetScale);
                 });
                 masterMarkerRenderer.render(container);
