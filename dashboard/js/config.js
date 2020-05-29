8971a236170e5e147f233b29ae0fdd3cb96d1e37
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-05-29 09:19:21 +0100

    reading the data from the github repo

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -7,8 +7,8 @@ function config() {
             cellBoundaries: './dashboard/cell_coords.json',
             cellData: './data/cell_call_demo_data/mouse_full_coronal/cell_type_output/cellData.json',
             tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_datastore/master/{z}/{y}/{x}.png',
-            spot_json: function(d){ return "./data/fov/" + 'fov_' + d + '/cell_type_out/fov_' + d + '_Dapi_overlays.json'},
-            cell_json: function(d){ return "./data/fov/" + 'fov_' + d + '/cell_type_out/fov_' + d + '_iss.json'},
+            spot_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/fov/" + 'fov_' + d + '/cell_type_out/fov_' + d + '_Dapi_overlays.json'},
+            cell_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/fov/" + 'fov_' + d + '/cell_type_out/fov_' + d + '_iss.json'},
         }, // 1
     ];
     var out = d3.map(ini, function (d) {
