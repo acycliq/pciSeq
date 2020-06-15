950caf4756f078231951045379a220511b8ca961
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-06-15 20:21:53 +0100

    pyramid tiles as jpg instead of png (smaller filesize)

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -6,7 +6,7 @@ function config() {
             imageSize: [262144, 194250],
             cellBoundaries: './dashboard/cell_coords_landscape.json',
             // cellData: './data/cell_call_demo_data/mouse_full_coronal/cell_type_output/cellData.json',
-            tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_landscape_datastore/master/{z}/{y}/{x}.png',
+            tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_jpg_datastore/master/262144px/{z}/{y}/{x}.jpg',
             spot_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'geneData_landscape_split/' + 'geneData_landscape_' + d + '.json'},
             cell_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'cellData_landscape_split/' + 'cellData_landscape_' + d + '.json'},
             num_jsons: 15 // number of json splits
