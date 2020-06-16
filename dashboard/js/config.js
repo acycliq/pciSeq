f117e8b60de076911e9c9dd87b778cb998355fe0
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-06-16 21:35:39 +0100

    minor edit

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -2,7 +2,7 @@ function config() {
     var ini = [
         {
             name: 'default',
-            roi: {"x0": 0, "x1": 27352, "y0": 0, "y1": 20268 }, // Now I am confused!! (ah ok, see note below)
+            roi: {"x0": 0, "x1": 27352, "y0": 0, "y1": 20268 },
             imageSize: [262144, 194250],
             cellBoundaries: './dashboard/cell_coords_landscape.json',
             // cellData: './data/cell_call_demo_data/mouse_full_coronal/cell_type_output/cellData.json',
@@ -17,9 +17,3 @@ function config() {
     });
     return out
 }
-
-//
-// ok here it is... map tiles and fov arent produced by the same image, that's why the background image and the data plotted are misaligned (I think!).
-// You padded the original image to make the longer side a multiple of 2000 whereas you just scaled it up to the correct dimension
-// when you did the pyramid tiles
-//
