5b6d8226db5fc0c048b12aee81742cf31238613e
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-06-30 21:31:50 +0100

    run with david's data (datatype: float64)

--- config.py
+++ config.py
@@ -56,3 +56,27 @@ MOUSE_FULL_CORONAL = {
     'exclude_genes': [], # Write here the genes you want to exclude from the cell calling algorithm
     'drop_nan': False,  # That is only a temp solution. I will be removing that when everything is set correctly, Well remove it now!!
 }
+
+DAVID = {
+    # Hyperparameters
+    'CellCallMaxIter': 100,
+    'CellCallTolerance': 0.02,
+    'Inefficiency': 0.2,
+    'InsideCellBonus': 2,
+    'MisreadDensity': 0.00001,
+    'SpotReg': 0.1,
+    'nNeighbors': 3,
+    'rGene': 20,
+    'rSpot': 2,
+    'max_iter': 100,
+
+    # Inputs to the cell calling algorithm
+    'scRNAseq': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'mouse_full_coronal', 'cell_type_input', 'scRNAseq.csv.gz'),
+    'saFile': os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'David', 'spots.csv'),  # Spot attributes, contains x,y coordinates for the spots and their gene names
+    'expanded_cells':  os.path.join(ROOT_DIR, 'data', 'cell_call_demo_data', 'David', 'cells.csv'),
+    'exclude_genes': [], # Write here the genes you want to exclude from the cell calling algorithm
+    'drop_nan': False,  # That is only a temp solution. I will be removing that when everything is set correctly, Well remove it now!!
+}
+
+
+"D:\Home\Dimitris\OneDrive - University College London\Data\David\Midbrain Sequencing Data\data\cell_call_demo_data\David\cells_david.csv"
