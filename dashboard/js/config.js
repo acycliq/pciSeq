c74d897e24b65fe53d11f7816bb1c8b81558b6af
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-07-02 19:57:24 +0100

    viewer for the new cell typed results

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -4,12 +4,15 @@ function config() {
             name: 'default',
             roi: {"x0": 0, "x1": 27352, "y0": 0, "y1": 20268 },
             imageSize: [262144, 194250],
-            cellBoundaries: './dashboard/cell_coords_landscape.json',
+            cellBoundaries: './dashboard/cell_coords_david.json',
             // cellData: './data/cell_call_demo_data/mouse_full_coronal/cell_type_output/cellData.json',
-            tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_jpg_datastore/master/262144px/{z}/{y}/{x}.jpg',
-            spot_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'geneData_landscape_split/' + 'geneData_landscape_' + d + '.json'},
-            cell_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'cellData_landscape_split/' + 'cellData_landscape_' + d + '.json'},
-            num_jsons: 15 // number of json splits
+            // tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_jpg_datastore/master/262144px/{z}/{y}/{x}.jpg',
+            tiles: './dashboard/img/262144px/{z}/{y}/{x}.png',
+            // spot_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'geneData_landscape_split/' + 'geneData_landscape_' + d + '.json'},
+            // cell_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'cellData_landscape_split/' + 'cellData_landscape_' + d + '.json'},
+            spot_json: function(d){ return './dashboard/img/json/David_full_brain/geneData_split/' + 'geneData_' + d + '.json'},
+            cell_json: function(d){ return './dashboard/img/json/David_full_brain/cellData_split/' + 'cellData_' + d + '.json'},
+            num_jsons: 25 // number of json splits
         }, // 1
     ];
     var out = d3.map(ini, function (d) {
