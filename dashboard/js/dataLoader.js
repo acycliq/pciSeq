3ac990db3742e677bd6db3ac955c0e653267c88a
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-07-31 09:39:36 +0100

    moved the bar variable into index.html

--- dashboard/js/dataLoader.js
+++ dashboard/js/dataLoader.js
@@ -76,39 +76,6 @@ function data_loader(workPackage) {
         }
     }
 
-    var bar = new ProgressBar.Circle(container, {
-        color: '#aaa',
-        // This has to be the same size as the maximum width to
-        // prevent clipping
-        strokeWidth: 4,
-        trailWidth: 1,
-        easing: 'easeInOut',
-        duration: 1400,
-        text: {
-            autoStyleContainer: false
-        },
-        style:{
-            color: '#ff0000',
-        },
-        from: {color: '#aaa', width: 1},
-        to: {color: '#333', width: 4},
-        // Set default step function for all animate calls
-        step: function (state, circle) {
-            circle.path.setAttribute('stroke', state.color);
-            circle.path.setAttribute('stroke-width', state.width);
-
-            var value = Math.round(circle.value() * 100);
-            if (value === 0) {
-                circle.setText('');
-            } else {
-                circle.setText(value);
-            }
-
-        }
-    });
-    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-    bar.text.style.fontSize = '2rem';
-
 
     function makeInnerHtml(data) {
         var innerHtml = "";
