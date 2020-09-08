2aeb604f6f7a22d8b5362158e06368386376aa55
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-09-08 22:54:45 +0100

    Finally!! It took me a whole day to fix that!

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -2,15 +2,14 @@ function config() {
     var ini = [
         {
             name: 'default',
-            roi: {"x0": 0, "x1": 27352, "y0": 0, "y1": 20268 }, // Now I am confused!! (ah ok, see note below)
+            roi: {"x0": 0, "x1": 27352, "y0": 0, "y1": 20268 },
             imageSize: [262144, 194250],
-            cellBoundaries: './dashboard/cell_coords_landscape.json',
             // cellData: './data/cell_call_demo_data/mouse_full_coronal/cell_type_output/cellData.json',
-            // tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_landscape_datastore/master/{z}/{y}/{x}.png',
             tiles: 'https://raw.githubusercontent.com/acycliq/full_coronal_jpg_datastore/master/262144px/{z}/{y}/{x}.jpg',
-            spot_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'geneData_landscape_split/' + 'geneData_landscape_' + d + '.json'},
-            cell_json: function(d){ return "https://raw.githubusercontent.com/acycliq/full_coronal_json_files/master/data/cell_call_demo_data/" + 'mouse_full_coronal/' + 'cell_type_output/' + 'cellData_landscape_split/' + 'cellData_landscape_' + d + '.json'},
-            num_jsons: 15 // number of json splits
+            cellData: 'https://api.github.com/repos/acycliq/full_coronal_json_files/contents/data/cell_call_demo_data/mouse_full_coronal/cell_type_output/tsv/cellData_split?ref=master',
+            geneData: 'https://api.github.com/repos/acycliq/full_coronal_json_files/contents/data/cell_call_demo_data/mouse_full_coronal/cell_type_output/tsv/geneData_split?ref=master',
+            cellCoords: 'https://api.github.com/repos/acycliq/full_coronal_json_files/contents/data/cell_call_demo_data/mouse_full_coronal/cell_type_output/tsv/cellCoords_split?ref=master',
+            class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
         }, // 1
     ];
     var out = d3.map(ini, function (d) {
@@ -18,9 +17,3 @@ function config() {
     });
     return out
 }
-
-//
-// ok here it is... map tiles and fov arent produced by the same image, that's why the background image and the data plotted are misaligned (I think!).
-// You padded the original image to make the longer side a multiple of 2000 whereas you just scaled it up to the correct dimension
-// when you did the pyramid tiles
-//
