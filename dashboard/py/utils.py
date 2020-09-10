2375dd169b670058eba4c6067deef46f41db93c7
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-09-10 14:50:10 +0100

    functions to split flat files into smaller pieces

---
+++ dashboard/py/utils.py
@@ -0,0 +1,90 @@
+""" Functions to split flat files """
+import pandas as pd
+import numpy as np
+import os
+import glob
+
+
+def splitter_mb(filepath, mb_size):
+    """ Splits a text file in (almost) equally sized parts on the disk. Assumes that there is a header in the first line
+    :param filepath: The path of the text file to be broken up into smaller files
+    :param mb_size: size in MB of each chunk
+    :return:
+    """
+    handle = open(filepath, 'r')
+    OUT_DIR = os.path.join(os.path.splitext(filepath)[0] + '_split')
+
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)
+    else:
+        files = glob.glob(OUT_DIR + '/*.*')
+        for f in files:
+            os.remove(f)
+
+    n = 0
+    header_line = next(handle)
+    file_out, handle_out = _get_file(OUT_DIR, filepath, n, header_line)
+    for line in handle:
+        size = os.stat(file_out).st_size
+        if size > mb_size*1024*1024:
+            print('saved %s with file size %4.3f MB' % (file_out, size/(1024*1024)))
+            n += 1
+            handle_out.close()
+            file_out, handle_out = _get_file(OUT_DIR, filepath, n, header_line)
+        handle_out.write(str(line))
+
+    # print(str(file_out) + " file size = \t" + str(size))
+    print('saved %s with file size %4.3f MB' % (file_out, size / (1024 * 1024)))
+    handle_out.close()
+
+
+def splitter_n(filepath, n):
+    """ Splits a text file in n smaller files
+    :param filepath: The path of the text file to be broken up into smaller files
+    :param n: determines how many smaller files will be created
+    :return:
+    """
+    filename_ext = os.path.basename(filepath)
+    [filename, ext] = filename_ext.split('.')
+
+    TARGET_FILE = filepath
+    OUT_DIR = os.path.join(os.path.splitext(filepath)[0] + '_split')
+
+    if ext == 'json':
+        df = pd.read_json(TARGET_FILE)
+    elif ext == 'tsv':
+        df = pd.read_csv(TARGET_FILE, sep='\t')
+    else:
+        df = None
+
+    df_list = np.array_split(df, n)
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)
+    else:
+        files = glob.glob(OUT_DIR + '/*.'+ext)
+        for f in files:
+            os.remove(f)
+
+    for i, d in enumerate(df_list):
+        fname = os.path.join(OUT_DIR, filename + '_%d.%s' % (i, ext))
+        if ext == 'json':
+            d.to_json(fname,  orient='records')
+        elif ext == 'tsv':
+            d.to_csv(fname, sep='\t', index=False)
+
+
+def _get_file(OUT_DIR, filepath, n, header_line):
+    [filename, ext] = os.path.basename(filepath).split('.')
+    file = os.path.join(OUT_DIR, filename + '_%d.%s' % (n, ext))
+    handle = open(file, "a")
+    handle.write(header_line)
+    return file, handle
+
+
+if __name__ == "__main__":
+    mb_size = 99
+    n = 4
+    filepaths = [r"geneData.tsv"]
+    for filepath in filepaths:
+        splitter_mb(filepath, mb_size)
+        splitter_n(filepath, n)
