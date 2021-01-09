0bfa3aa430b8cf89ea68e6de8e495d0b927446bb
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-01-09 09:40:45 +0000

    clean-up

--- src/preprocess/spot_labels.py
+++ src/preprocess/spot_labels.py
@@ -17,103 +17,6 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 logger = logging.getLogger()
 
 
-def coofy(spots, label_image):
-# FUNCTION DEPRECATED
-    x = spots.x.values.astype(int)
-    y = spots.y.values.astype(int)
-
-    coo_shape = label_image.shape
-    idx = spots.index.values + 1  # avoid having idx = 0
-    coo = coo_matrix((idx, (y, x)), shape=coo_shape)
-    return coo
-
-
-def _spot_parent_label(spots, sp_label_image):
-# DEPRECATED. Replaced by inside_cell()
-
-    if sp_label_image.nnz == 0:
-        # The tile empty, all spots are on the background
-        spots['label'] = 0
-    else:
-        # 1. get the label_image
-        label_image = sp_label_image.toarray()
-
-        # 2. unscaled and local coordinates for the spots (as a sparse array)
-        coo = coofy(spots.copy(), label_image)
-
-        coo_arr = coo.toarray()
-        label_coords = coo_matrix(label_image * coo_arr.astype(bool))
-        spot_id_coords = coo_matrix(label_image.astype(bool) * coo_arr)
-
-        df = pd.DataFrame({'x': label_coords.col,
-                           'y': label_coords.row,
-                           'label': label_coords.data},
-                          index=spot_id_coords.data).astype(int)
-
-        if np.any(df.index.duplicated()):
-            print('Found %d duplicated. Investigate!' % df.index.duplicated().sum())
-            ## that actually means that the same spot (ie read/dot) exists at two different locations at the same time
-
-        df = df[~df.index.duplicated()]
-        # spots['label'] = df.label
-        spots = spots.merge(df, how='left', on=['x', 'y'])
-
-        # if nan it means the spot is on the background. Hence set the label = 0
-        spots['label'] = spots.label.fillna(0).astype(int)
-    return spots
-
-
-def writer(cells, spots, dirpath):
-    if os.path.exists(dirpath) and os.path.isdir(dirpath):
-        shutil.rmtree(dirpath)
-    os.mkdir(dirpath)
-
-    writer_cells(cells, dirpath)
-    writer_spots(spots, dirpath)
-
-
-def writer_cells(cell_props, dirpath):
-    '''
-    save the data to the flatfile
-    :return:
-    '''
-
-    cell_props = cell_props.sort_values(by=['label', 'x_cell', 'y_cell'])
-
-    # 1. save the cell props
-    cell_props = cell_props.rename({'x_cell': 'x', 'y_cell': 'y'}, axis=1)
-    # cell_props['x'] = cell_props.x.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
-    # cell_props['y'] = cell_props.y.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
-    cell_props['cell_id'] = cell_props.cell_id.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
-    cell_props['label'] = cell_props.label.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
-
-    cells_headers = ['cell_id', 'label', 'area', 'x', 'y']
-    cell_props[cells_headers].to_csv(os.path.join(dirpath, '_cells.csv'), index=False)
-
-    # 2. save the cell coords
-    coords_headers = ['cell_id', 'label', 'coords']
-    cell_props[coords_headers].to_json(os.path.join(dirpath, '_cellCoords.json'), orient='records')
-    return cell_props[cells_headers], cell_props[coords_headers]
-
-
-def writer_spots(spots_df, dirpath):
-
-    # spots_df = spots_df.sort_values(by=['label', 'x', 'y'])
-
-    # 3. save the spots
-    spots_df['target'] = spots_df.Gene
-    spots_df['x_global'] = spots_df.x
-    spots_df['y_global'] = spots_df.y
-    # spots_df['x_cell'] = spots_df.x_cell.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
-    # spots_df['y_cell'] = spots_df.y_cell.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
-
-    spots_headers = ['x_global', 'y_global', 'label', 'target', 'x_cell', 'y_cell']
-    spots_df[spots_headers].to_csv(os.path.join(dirpath, '_spots.csv'), index=False)
-    print('Total number of collected spots: %d' % spots_df.shape[0])
-
-    return spots_df[spots_headers]
-
-
 def inside_cell(a, idx):
     '''
     Given an array a (image_array) and
@@ -220,4 +123,101 @@ def stage_data(cfg):
 
     dirpath = cfg['temp']
     writer(cells, spots, dirpath)
+
+
+def writer(cells, spots, dirpath):
+    if os.path.exists(dirpath) and os.path.isdir(dirpath):
+        shutil.rmtree(dirpath)
+    os.mkdir(dirpath)
+
+    writer_cells(cells, dirpath)
+    writer_spots(spots, dirpath)
+
+
+def writer_cells(cell_props, dirpath):
+    '''
+    save the data to the flatfile
+    :return:
+    '''
+
+    cell_props = cell_props.sort_values(by=['label', 'x_cell', 'y_cell'])
+
+    # 1. save the cell props
+    cell_props = cell_props.rename({'x_cell': 'x', 'y_cell': 'y'}, axis=1)
+    # cell_props['x'] = cell_props.x.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
+    # cell_props['y'] = cell_props.y.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
+    cell_props['cell_id'] = cell_props.cell_id.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
+    cell_props['label'] = cell_props.label.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
+
+    cells_headers = ['cell_id', 'label', 'area', 'x', 'y']
+    cell_props[cells_headers].to_csv(os.path.join(dirpath, '_cells.csv'), index=False)
+
+    # 2. save the cell coords
+    coords_headers = ['cell_id', 'label', 'coords']
+    cell_props[coords_headers].to_json(os.path.join(dirpath, '_cellCoords.json'), orient='records')
+    return cell_props[cells_headers], cell_props[coords_headers]
+
+
+def writer_spots(spots_df, dirpath):
+
+    # spots_df = spots_df.sort_values(by=['label', 'x', 'y'])
+
+    # 3. save the spots
+    spots_df['target'] = spots_df.Gene
+    spots_df['x_global'] = spots_df.x
+    spots_df['y_global'] = spots_df.y
+    # spots_df['x_cell'] = spots_df.x_cell.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
+    # spots_df['y_cell'] = spots_df.y_cell.fillna(-1).astype(int).astype('str').replace('-1', np.nan)
+
+    spots_headers = ['x_global', 'y_global', 'label', 'target', 'x_cell', 'y_cell']
+    spots_df[spots_headers].to_csv(os.path.join(dirpath, '_spots.csv'), index=False)
+    print('Total number of collected spots: %d' % spots_df.shape[0])
+
+    return spots_df[spots_headers]
+
+### FUNCIONS BELOW ARE DEPRECATED ###
+def coofy(spots, label_image):
+# FUNCTION DEPRECATED
+    x = spots.x.values.astype(int)
+    y = spots.y.values.astype(int)
+
+    coo_shape = label_image.shape
+    idx = spots.index.values + 1  # avoid having idx = 0
+    coo = coo_matrix((idx, (y, x)), shape=coo_shape)
+    return coo
+
+
+def _spot_parent_label(spots, sp_label_image):
+# DEPRECATED. Replaced by inside_cell()
+
+    if sp_label_image.nnz == 0:
+        # The tile empty, all spots are on the background
+        spots['label'] = 0
+    else:
+        # 1. get the label_image
+        label_image = sp_label_image.toarray()
+
+        # 2. unscaled and local coordinates for the spots (as a sparse array)
+        coo = coofy(spots.copy(), label_image)
+
+        coo_arr = coo.toarray()
+        label_coords = coo_matrix(label_image * coo_arr.astype(bool))
+        spot_id_coords = coo_matrix(label_image.astype(bool) * coo_arr)
+
+        df = pd.DataFrame({'x': label_coords.col,
+                           'y': label_coords.row,
+                           'label': label_coords.data},
+                          index=spot_id_coords.data).astype(int)
+
+        if np.any(df.index.duplicated()):
+            print('Found %d duplicated. Investigate!' % df.index.duplicated().sum())
+            ## that actually means that the same spot (ie read/dot) exists at two different locations at the same time
+
+        df = df[~df.index.duplicated()]
+        # spots['label'] = df.label
+        spots = spots.merge(df, how='left', on=['x', 'y'])
+
+        # if nan it means the spot is on the background. Hence set the label = 0
+        spots['label'] = spots.label.fillna(0).astype(int)
+    return spots
 
