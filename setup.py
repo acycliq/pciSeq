081d75f7be1c0fee1561ec03c986947506e6ef9d
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-01-15 04:06:06 +0000

    bug fixing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pciSeq",
-    version="0.0.2",
+    version="0.0.21",
     license="BSD",
     author="Dimitris Nicoloutsopoulos",
     author_email="dimitris.nicoloutsopoulos@gmail.com",
