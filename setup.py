4eb18d6fe56ace50ed086c39c6f220ff43950e59
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-04-08 16:20:09 +0100

    version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pciSeq",
-    version="0.0.30",
+    version="0.0.31",
     license="BSD",
     author="Dimitris Nicoloutsopoulos",
     author_email="dimitris.nicoloutsopoulos@gmail.com",
