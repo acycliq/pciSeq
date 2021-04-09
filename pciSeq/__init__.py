2c22cb204f33ec48e5d8f25fa50b4c69c8b07f47
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-04-09 23:44:11 +0100

    clean-up

--- pciSeq/__init__.py
+++ pciSeq/__init__.py
@@ -2,10 +2,4 @@ from pciSeq.app import fit
 from pciSeq.app import cell_type
 from pciSeq.src.preprocess.spot_labels import stage_data
 import pciSeq.src.cell_call.utils as utils
-import json
-from os.path import dirname
 
-with open(dirname(__file__) + '/version.json') as fp:
-    _info = json.load(fp)
-
-__version__ = _info['version']
