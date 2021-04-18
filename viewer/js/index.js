37912f1e0d763aa593529df6104ceff740a50a37
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-04-18 10:51:27 +0100

    removed taxonomy field from datatable

--- viewer/js/index.js
+++ viewer/js/index.js
@@ -97,7 +97,7 @@ function legendControl() {
     if (!legend_added) {
         legendLink.addEventListener(`click`, () => {
             // Opens the page and stores the opened window instance
-            legendWindow = window.open(`./dashboard/genes_datatable.html`); // <--- THAT NEEDS TO BE EXPOSED TO THE USER. MOVE I INSIDE config.js MAYBE
+            legendWindow = window.open(`./viewer/genes_datatable.html`); // <--- THAT NEEDS TO BE EXPOSED TO THE USER. MOVE I INSIDE config.js MAYBE
             // legendWindow = window.open('./viewer/genes_datatable.html', '_blank','toolbar=yes');
 
         });
