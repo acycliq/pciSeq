ade5b7a8dd52e71cb4a377e0a8a50e6e59f2b0ea
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-09-11 23:53:09 +0100

    minor edit

--- pciSeq/src/cell_call/main.py
+++ pciSeq/src/cell_call/main.py
@@ -254,7 +254,8 @@ class VarBayes:
         spot to cell assignment.
         Implements equation (4) of the Qian paper
         """
-        nN = self.config['nNeighbors'] + 1
+        # nN = self.config['nNeighbors'] + 1
+        nN = self.nN
         nS = self.spots.data.gene_name.shape[0]
 
         # initialise array with the misread density
