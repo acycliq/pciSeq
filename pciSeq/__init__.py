f3fa7a1f73eaa0623f7fadbc870b7a66ac2ca39b
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-02-20 19:34:37 +0000

    minor edit

--- pciSeq/__init__.py
+++ pciSeq/__init__.py
@@ -43,10 +43,6 @@ def check_libvips():
         import pyvips
         status = True
     except OSError:
-        # logger.warning('>>>> libvips is not installed. Please see https://www.libvips.org/install.html <<<<')
-        # logger.warning('>>>> This is required if you want to do your own viewer. <<<<')
-        # logger.warning('>>>> and visualise your results after cell typing. <<<<')
-        # logger.warning('>>>> To do cell typing, libvips can be ignored, it *is not* necessary.  <<<<')
         status = False
     except Exception as err:
         raise
