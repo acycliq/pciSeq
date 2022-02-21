ace673412bc83e6b053b003176457de1129831b4
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-02-21 00:45:47 +0000

    minor edit

--- pciSeq/__init__.py
+++ pciSeq/__init__.py
@@ -57,12 +57,12 @@ import pciSeq.src.cell_call.utils as utils
 
 if check_libvips():
     from pciSeq.src.viewer.stage_image import tile_maker
-else:
-    logger.warning('>>>> libvips is not installed. Please see https://www.libvips.org/install.html <<<<')
-    logger.warning('>>>> This is required if you want to do your own viewer. <<<<')
-    logger.warning('>>>> and visualise your results after cell typing. <<<<')
-    logger.warning('>>>> To do cell typing, libvips can be ignored, it is *not* necessary.  <<<<')
-    logger.warning('>>>> LIBVIPS_ENABLED is %s.  <<<<' % check_libvips())
+# else:
+#     logger.warning('>>>> libvips is not installed. Please see https://www.libvips.org/install.html <<<<')
+#     logger.warning('>>>> This is required if you want to do your own viewer. <<<<')
+#     logger.warning('>>>> and visualise your results after cell typing. <<<<')
+#     logger.warning('>>>> To do cell typing, libvips can be ignored, it is *not* necessary.  <<<<')
+#     logger.warning('>>>> LIBVIPS_ENABLED is %s.  <<<<' % check_libvips())
 
 
 
