66bfbcba9ef916c900e05f4d822bc9f0e54350d9
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-06-23 01:24:26 +0100

    v0.0.43

--- pciSeq/__init__.py
+++ pciSeq/__init__.py
@@ -54,6 +54,7 @@ from pciSeq.app import fit
 from pciSeq.app import cell_type
 from pciSeq.src.preprocess.spot_labels import stage_data
 import pciSeq.src.cell_call.utils as utils
+from pciSeq.src.cell_call.log_config import attach_to_log
 
 if check_libvips():
     from pciSeq.src.viewer.stage_image import tile_maker
