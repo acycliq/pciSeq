00c804a8cf6f710f70b20c992003adbe4b143c59
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-09-05 17:03:25 +0100

    barchart gets updated inside a loop

--- pciSeq/src/diagnostics/diagnostics.py
+++ pciSeq/src/diagnostics/diagnostics.py
@@ -1,5 +1,8 @@
 import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
+import numpy as np
+import time
+from pciSeq.app import run_me
 
 st.set_page_config(
     page_title="Real-Time Data Science Dashboard",
@@ -8,23 +11,25 @@ st.set_page_config(
 )
 
 df = px.data.tips()
-
 # dashboard title
 st.title("pciSeq: Diagnostics.")
 
 # creating a single-element container
 placeholder = st.empty()
 
-with placeholder.container():
-    # create two columns for charts
-    fig_col1, fig_col2 = st.columns(2)
-    with fig_col1:
-        st.markdown("### Second Chart")
-        fig1 = px.histogram(df, x="total_bill")
-        # fig2 = px.histogram(data_frame=df, x="age_new")
-        st.write(fig1)
+for seconds in range(200):
+    df['random_bill'] = 100 * np.random.random_sample(df.shape[0])
+    with placeholder.container():
+        # create two columns for charts
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### Chart Num %d" % seconds)
+            fig1 = px.histogram(df, x="random_bill")
+            # fig2 = px.histogram(data_frame=df, x="age_new")
+            st.write(fig1)
 
 
-st.dataframe(df)
+    # st.dataframe(df)
+    time.sleep(1)
 
 
