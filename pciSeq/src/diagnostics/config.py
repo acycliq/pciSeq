14006a1d97dabd5f61e53d715867b04d84732f4b
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-09-22 23:54:10 +0100

    saving the db to the temp folder

--- pciSeq/src/diagnostics/config.py
+++ pciSeq/src/diagnostics/config.py
@@ -1,6 +1,11 @@
+from pciSeq.src.cell_call.utils import get_out_dir
+import pciSeq.config as pciSeq_cfg
+import os
+
 
 SETTINGS = {
     # 'DB_URL': "file:memdb1?mode=memory&cache=shared"
-    'DB_URL': r'D:\Home\Dimitris\OneDrive - University College London\dev\Python\pciSeq\pciSeq\my_pciSeq.db'
+    # 'DB_URL': r'D:\Home\Dimitris\OneDrive - University College London\dev\Python\pciSeq\pciSeq\my_pciSeq.db',
+    'DB_URL': os.path.join(get_out_dir(pciSeq_cfg.DEFAULT['output_path'], ''), 'pciSeq.db')
 }
 
