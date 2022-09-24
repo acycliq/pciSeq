78e9dc8ecd0c63b14fc91bd68dd8224d3a95d4e4
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-09-24 19:35:14 +0100

    minor edit

--- pciSeq/src/diagnostics/config.py
+++ pciSeq/src/diagnostics/config.py
@@ -4,8 +4,6 @@ import os
 
 
 SETTINGS = {
-    # 'DB_URL': "file:memdb1?mode=memory&cache=shared"
-    # 'DB_URL': r'D:\Home\Dimitris\OneDrive - University College London\dev\Python\pciSeq\pciSeq\my_pciSeq.db',
     'DB_URL': os.path.join(get_out_dir(pciSeq_cfg.DEFAULT['output_path'], ''), 'pciSeq.db')
 }
 
