1e2e4f44cdbcb0f084d742c32955650506cadaa7
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-11-14 23:28:50 +0000

    installing memurai (redis counterpart in Windows) if it is missing

--- pciSeq/src/diagnostics/utils.py
+++ pciSeq/src/diagnostics/utils.py
@@ -1,6 +1,11 @@
+import os
 import redis
 import pickle
+from ctypes import windll
+from pciSeq.src.cell_call.log_config import logger
 import pciSeq.src.diagnostics.config as config
+import subprocess
+import sysconfig
 
 
 class redis_db():
@@ -14,6 +19,7 @@ class redis_db():
     def _attach_client(self):
         self.pool = config.SETTINGS['POOL']
         self.redis_client = redis.Redis(connection_pool=self.pool)
+        assert self.is_connected()
 
     def to_redis(self, df_in, key, **kwargs):
         df = df_in.copy()
@@ -30,6 +36,23 @@ class redis_db():
 
     def _flushdb(self):
         self.redis_client.flushdb()
+
+    def is_connected(self):
+        print(os.getcwd())
+        try:
+            return self.redis_client.ping()
+        except (redis.exceptions.ConnectionError, ConnectionRefusedError):
+            logger.info("Redis ping failed!. Trying to install redis server")
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), )
+            msi = os.path.join(sysconfig.get_path('purelib'), 'pciSeq', 'static', 'memurai', 'Memurai-Developer-v3.1.4.msi')
+            if not os.path.isfile(msi):
+                msi = os.path.join(os.getcwd(), 'static', 'memurai', 'Memurai-Developer-v3.1.4.msi')
+            logger.info("Calling %s" % msi)
+            exit_code = subprocess.call(msi, shell=True)  # returns the exit code in unix. if 0 then success
+            if exit_code > 0:
+                raise Exception("Installation failed with exit code: %d" % exit_code)
+            return exit_code == 0
+
 
 
 
