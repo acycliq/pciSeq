dc8c74e17c81b7183aa89587b465d067ff532c3a
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-01-27 12:42:19 +0000

    changed the path so code can be run under WSL

--- pciSeq/app.py
+++ pciSeq/app.py
@@ -341,11 +341,11 @@ def run_me():
     _scRNAseq = _scRNAseq.astype(float).astype(np.uint32)
 
     # # read 3D some demo data
-    _iss_spots_3D = pd.read_csv(r"e:/data/Anne/220308_50umCF_seq_atto425_DY520XL_MS002/spots_yxz.csv")
+    _iss_spots_3D = pd.read_csv(r"/mnt/e/data/Anne/220308_50umCF_seq_atto425_DY520XL_MS002/spots_yxz.csv")
     _iss_spots_3D = _iss_spots_3D.assign(z_stack=_iss_spots_3D.z)
     _iss_spots_3D = _iss_spots_3D[['y', 'x', 'z_stack', 'Gene']]
     # _iss_spots = _iss_spots.assign(z=_iss_spots.z_stack * config.DEFAULT['anisotropy'])
-    _coo_3D = np.load(r"e:/data/Anne/220308_50umCF_seq_atto425_DY520XL_MS002/masks_2D_stiched_fullsize.npz", allow_pickle=True)['arr_0']
+    _coo_3D = np.load(r"/mnt/e/data/Anne/220308_50umCF_seq_atto425_DY520XL_MS002/masks_2D_stiched_fullsize.npz", allow_pickle=True)['arr_0']
     # _coo_3D = np.load("/mnt/e/data/Mathieu/dapi_segmented_restitched.npy")
     _coo_3D = [coo_matrix(d) for d in _coo_3D]
 
