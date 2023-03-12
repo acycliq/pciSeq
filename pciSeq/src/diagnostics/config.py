cbba43de449eb4a2188eae283ba068b34e154a1c
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-03-12 10:32:37 +0000

    merging diagnostics code from dev_3d

---
+++ pciSeq/src/diagnostics/config.py
@@ -0,0 +1,11 @@
+from pciSeq.src.cell_call.utils import get_out_dir
+import pciSeq.config as pciSeq_cfg
+import redis
+import os
+
+
+SETTINGS = {
+    # 'DB_URL': os.path.join(get_out_dir(pciSeq_cfg._BASE['output_path'], ''), 'pciSeq.db'),
+    'POOL': redis.ConnectionPool(host='localhost', port=6379, db=0)
+}
+
---
+++ pciSeq/src/diagnostics/launch_diagnostics.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+from pciSeq.src.cell_call.log_config import logger, attach_to_log
+import pciSeq.src.diagnostics.utils as utils
+
+
+def launch_dashboard():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'diagnostics.py')
+    exe = sys.executable
+    code_1, code_2 = utils.validate()
+    if code_1 == 0 and code_2 == 0:
+        p = subprocess.Popen([exe, "-m" "streamlit", "run", filename])
+        # TODO: you need to kill the process on exit
+        # logger.info('Starting process with pid: %d to run the diagnostics' % p.pid)
+    else:
+        logger.info("Skipping diagnostics, cannot run them. Either redis not installed or not running.")
+
+
+if __name__ == "__main__":
+    attach_to_log()
+    launch_dashboard()
