c5781f75e05e119c3a69069f70cb50b0d6cf73a8
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-06-06 00:03:58 +0100

    correct paths and some comments

--- pciSeq/static/2D/viewer/js/config.js
+++ pciSeq/static/2D/viewer/js/config.js
@@ -2,8 +2,10 @@
 // 1. roi is the image size in pixels. Leave x0 and y0 at zero and set x1 to the width and y1 to the height
 // 2. zoomLevels, leave this at 10
 // 3. tiles should point to the folder that keeps your pyramid of tiles. If you do not have that just
-//    change the link to a blind one (change the jpg extension for example). The viewer should work
-//    without the dapi background though
+//    change the link to a blind one (use an empty string or change the jpg extension for example).
+//    The viewer should work without the dapi background.
+//    If the tiles are stored locally, they should be stored under the project root dir and the link
+//    should be the path from the project root dir too.
 // 4. cellData, geneData, cellBoundaries:
 //    4.1: mediaLink paths are with respect to the location of 'streaming-tsv-parser.js'
 //    4.2: size is the tsv size in bytes. I use os.path.getsize() to get it. Not crucial if you dont get it right,
@@ -15,9 +17,9 @@ function config() {
             roi: {"x0": 0, "x1": 7602, "y0": 0, "y1": 5471},
             zoomLevels: 10, // maximum zoom levels. Leave that at 10.
             tiles: 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{y}/{x}.jpg',
-            cellData: {mediaLink: '../data/cellData.tsv', size: "2179232"},
-            geneData: {mediaLink: '../data/geneData.tsv', size: "9630856"},
-            cellBoundaries: {mediaLink: '../data/cellBoundaries.tsv', size: "1306209"},
+            cellData: {mediaLink: '../../data/cellData.tsv', size: "2179232"},
+            geneData: {mediaLink: '../../data/geneData.tsv', size: "9630856"},
+            cellBoundaries: {mediaLink: '../../data/cellBoundaries.tsv', size: "1306209"},
         }
 }
 
