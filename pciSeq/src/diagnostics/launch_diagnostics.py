3ee4c89267afc8e5f9f00911e8865267445059d3
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-12-20 23:18:32 +0000

    minor edit

--- pciSeq/src/diagnostics/launch_diagnostics.py
+++ pciSeq/src/diagnostics/launch_diagnostics.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import subprocess
 from pciSeq.src.core.log_config import logger, attach_to_log
 import pciSeq.src.diagnostics.utils as utils
@@ -8,8 +7,6 @@ import pciSeq.src.diagnostics.utils as utils
 def launch_dashboard():
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'diagnostics.py')
-    # redirect to dev/null to bypass the welcome message from streamlit
-    # devnull = '> /dev/null' if sys.platform in ['linux', 'darwin'] else '> NUL'
     code_1, code_2 = utils.validate_redis()
     if code_1 == 0 and code_2 == 0:
         p = subprocess.Popen(["streamlit", "run", filename, " --server.headless true"])
