58d948bc47c80d415c572576333e0eb7e73ea64f
dimitris <dimitris.nicoloutsopoulos@gmail.com>
2024-02-02 15:33:53 +0000

    clean up

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,8 @@
 import os
 import pytest
-import tempfile, shutil
+import tempfile
 import pandas as pd
-import numpy as np
-import fastremap
-import skimage.measure as skmeas
-from scipy.sparse import coo_matrix, save_npz, load_npz
+from scipy.sparse import load_npz
 import logging
 
 gtd_logger = logging.getLogger(__name__)
@@ -25,14 +22,6 @@ def pytest_configure():
     pytest.fcells = None
     pytest.fscData = None
 
-@pytest.fixture
-def get_test_data():
-    spots, image_label = make_test_data(get_out_dir(), bbox=None)
-    coo = coo_matrix(image_label)
-
-    scRNAseq = pd.read_csv('test_scRNAseq.csv').set_index('gene_name')
-    return spots, coo, scRNAseq
-
 
 def get_out_dir():
     out_dir = os.path.join(tempfile.gettempdir(), 'pciSeq', 'tests')
@@ -41,50 +30,22 @@ def get_out_dir():
     return out_dir
 
 
-def make_test_data(test_data_dir, bbox=None):
-    image_label = read_segmentation()
-    if bbox is None:
-        bbox = [
-            (0, 0),                     # bottomleft, [x0, y0]
-            image_label.shape[::-1]     # topright, [x1, y1]
-        ]
-    props = skmeas.regionprops_table(image_label, properties=['label', 'centroid'])
-    props_df = pd.DataFrame(props).rename(columns={'centroid-0': 'y', 'centroid-1': 'x'})
+@pytest.fixture
+def read_demo_data(bbox=None):
+    path_str = os.path.join('..', 'pciSeq', 'data', 'mouse', 'ca1')
+    spots = pd.read_csv(os.path.join(path_str, 'iss', 'spots.csv'))
+    label_image = load_npz(os.path.join(path_str, 'segmentation', 'label_image.coo.npz'))
 
-    image_label, props_df = clip_image(image_label.copy(), props_df.copy(), bbox)
-    coo_file = os.path.join(test_data_dir, 'test_label_image_coo.npz')
-    save_npz(coo_file, coo_matrix(image_label))
-    gtd_logger.info("saved at: %s" % coo_file)
-
-    spots = read_spots()
-    spots = clip_spots(spots.copy(), bbox)
-    spots_file = os.path.join(test_data_dir, 'test_spot_data.csv')
-    spots.to_csv(spots_file, index=False)
-    gtd_logger.info("saved at: %s" % spots_file)
-
-    return spots, image_label
+    scData = pd.read_csv('test_scRNAseq.csv').set_index('gene_name')
+    if bbox is not None:
+        spots, label_image = clip_data(spots.copy(), label_image.copy, bbox)
+    return spots, label_image, scData
 
 
-def read_segmentation():
-    coo = load_npz(os.path.join('..', 'pciSeq', 'data', 'mouse', 'ca1', 'segmentation', 'label_image.coo.npz'))
-    return coo.toarray().astype(np.int32)
-
-
-def clip_image(im, im_props, bbox):
-    im_props = clip_dataframe(im_props.copy(), bbox)
-
-    # Clip the label image between the bounding box (4238, 364) and (5160, 933)
-    x0, y0 = bbox[0]
-    x1, y1 = bbox[1]
-    image_label = im[y0:y1+1, x0:x1+1]
-    image_label = fastremap.mask_except(image_label, im_props.label.tolist())
-    return image_label, im_props
-
-
-def read_spots():
-    # Load now the spots
-    spots = pd.read_csv(os.path.join('..', 'pciSeq', 'data', 'mouse', 'ca1', 'iss', 'spots.csv'))
-    return spots
+def clip_data(spots, img, bbox):
+    spots_out = clip_spots(spots, bbox)
+    img_out = clip_label_image(img, bbox)
+    return spots_out, img_out
 
 
 def clip_spots(spots, bbox):
@@ -100,8 +61,17 @@ def clip_spots(spots, bbox):
 def clip_dataframe(df, bbox):
     x0, y0 = bbox[0]
     x1, y1 = bbox[1]
-    idx_x = (df.x > x0) & (df.x < x1)
-    idx_y = (df.y > y0) & (df.y < y1)
+    idx_x = (df.x >= x0) & (df.x <= x1)
+    idx_y = (df.y >= y0) & (df.y <= y1)
 
     idx = idx_x & idx_y
     return df[idx]
+
+
+def clip_label_image(im, bbox):
+    x0, y0 = bbox[0]
+    x1, y1 = bbox[1]
+    return im[y0:y1, x0:x1]
+
+
+
