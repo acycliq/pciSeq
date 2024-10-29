c066a0b0b018bf4f4d4252dd433927f93978e92a
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2024-10-29 12:50:35 +0000

    first commit

---
+++ cell_analysis/distanceProbabilityPlot.js
@@ -0,0 +1,103 @@
+export class DistanceProbabilityPlot {
+    constructor(containerId, data) {
+        this.containerId = containerId;
+        this.data = data;
+        this.margin = {top: 60, right: 80, bottom: 50, left: 100};
+        this.initializePlot();
+    }
+
+    initializePlot() {
+        // Get container width and height
+        const container = d3.select(`#${this.containerId}`);
+        const width = window.innerWidth - this.margin.left - this.margin.right;
+        const height = window.innerHeight * 0.34 - this.margin.top - this.margin.bottom;
+
+        // Create SVG
+        this.svg = container.append('svg')
+            .attr('width', width + this.margin.left + this.margin.right)
+            .attr('height', height + this.margin.top + this.margin.bottom)
+            .append('g')
+            .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
+
+        // Set up scales
+        this.x = d3.scaleLinear()
+            .domain(d3.extent(this.data.x))
+            .range([0, width]);
+
+        this.y = d3.scaleLinear()
+            .domain(d3.extent(this.data.y))
+            .range([height, 0]);
+
+        // Add axes
+        this.svg.append('g')
+            .attr('transform', `translate(0,${height})`)
+            .call(d3.axisBottom(this.x));
+
+        this.svg.append('g')
+            .call(d3.axisLeft(this.y));
+
+        // Add labels
+        this.svg.append("text")
+            .attr("x", width / 2)
+            .attr("y", height + this.margin.bottom - 10)
+            .style("text-anchor", "middle")
+            .text(this.data.xlabel || "Distance");
+
+        this.svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -height / 2)
+            .attr("y", -this.margin.left + 40)
+            .style("text-anchor", "middle")
+            .text(this.data.ylabel || "Probability");
+
+        // Create tooltip
+        this.tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        this.updatePlot();
+    }
+
+    updatePlot() {
+        // Create points data
+        const points = this.data.x.map((d, i) => ({
+            x: d,
+            y: this.data.y[i],
+            label: this.data.labels[i]
+        }));
+
+        // Add points
+        const dots = this.svg.selectAll('circle')
+            .data(points);
+
+        // Enter new points
+        dots.enter()
+            .append('circle')
+            .attr('r', 4)
+            .style('fill', '#69b3a2')
+            .merge(dots)
+            .attr('cx', d => this.x(d.x))
+            .attr('cy', d => this.y(d.y))
+            .on('mouseover', (event, d) => {
+                this.tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                this.tooltip.html(`Gene: ${d.label}<br/>Distance: ${d.x.toFixed(2)}<br/>Probability: ${d.y.toFixed(2)}`)
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+            })
+            .on('mouseout', () => {
+                this.tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+        // Remove old points
+        dots.exit().remove();
+    }
+
+    resize() {
+        // Implement resize logic here if needed
+        // Similar to the existing ScatterPlot resize logic
+    }
+}
