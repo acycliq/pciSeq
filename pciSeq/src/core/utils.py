a449e13c874228f6302d9c9a43ebf05ee2efcfc6
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2024-11-16 19:30:37 +0000

    refactor: reorganised code

--- pciSeq/src/core/utils.py
+++ pciSeq/src/core/utils.py
@@ -10,11 +10,13 @@ from ... import config
 from ..diagnostics.utils import check_redis_server
 import os
 import glob
-import subprocess
+from numbers import Number
+from scipy.sparse import coo_matrix
 import numbers
-from email.parser import BytesHeaderParser
-from scipy.spatial import cKDTree
-import webbrowser
+from ...src.viewer.utils import (copy_viewer_code, make_config_js,
+                                     make_classConfig_js, make_glyphConfig_js,
+                                     make_classConfig_nsc_js, build_pointcloud,
+                                     cellData_rgb, cell_gene_counts)
 import logging
 
 utils_logger = logging.getLogger(__name__)
@@ -572,4 +574,208 @@ def export_db_table(table_name, out_dir, con):
     fname = os.path.join(out_dir, table_name + '.csv')
     df.to_csv(fname, index=False)
     utils_logger.info('Saved at %s' % fname)
+
+
+def write_data(cellData, geneData, cellBoundaries, varBayes, cfg):
+    dst = get_out_dir(cfg['output_path'])
+    out_dir = os.path.join(dst, 'data')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    cellData.to_csv(os.path.join(out_dir, 'cellData.tsv'), sep='\t', index=False)
+    utils_logger.info('Saved at %s' % (os.path.join(out_dir, 'cellData.tsv')))
+
+    geneData.to_csv(os.path.join(out_dir, 'geneData.tsv'), sep='\t', index=False)
+    utils_logger.info('Saved at %s' % (os.path.join(out_dir, 'geneData.tsv')))
+
+    # Do not change the if-then branching flow. InsideCellBonus can take the value of zero
+    # and the logic below will ensure that in that case, the segmentation borders will be
+    # drawn instead of the gaussian contours.
+    if cfg['InsideCellBonus'] is False:
+        ellipsoidBoundaries = cellData[['Cell_Num', 'gaussian_contour']]
+        ellipsoidBoundaries = ellipsoidBoundaries.rename(columns={"Cell_Num": "cell_id", "gaussian_contour": "coords"})
+        ellipsoidBoundaries.to_csv(os.path.join(out_dir, 'cellBoundaries.tsv'), sep='\t', index=False)
+        utils_logger.info(' Saved at %s' % (os.path.join(out_dir, 'cellBoundaries.tsv')))
+    else:
+        cellBoundaries.to_csv(os.path.join(out_dir, 'cellBoundaries.tsv'), sep='\t', index=False)
+        utils_logger.info('Saved at %s' % (os.path.join(out_dir, 'cellBoundaries.tsv')))
+
+    serialise(varBayes, os.path.join(out_dir, 'debug'))
+
+
+def validate(spots, coo, scData, cfg):
+
+    # check the label image
+    coo = _validate_coo(coo)
+
+    # Check now the config dict. It needs to be done after we have checked the coo
+    # because the shape of the label image determines whether the data is 3D or 2D.
+    cfg = _validate_cfg(cfg, coo)
+
+    # check the spots and see if all genes are in your single cell library
+    spots = _validate_spots(spots, coo, scData, cfg)
+
+    # check single cell data
+    if scData is not None:
+        assert isinstance(scData,
+                          pd.DataFrame), "Single cell data should be passed-in to the fit() method as a dataframe"
+
+    # remove genes that cannot been found in the single cell data
+    spots = clean_spots(spots, scData)
+
+    # do some datatype casting
+    spots = spots.astype({
+        'gene_name': str,
+        'x': np.float32,
+        'y': np.float32,
+        'z_plane': np.float32})
+
+    if isinstance(cfg['MisreadDensity'], Number):
+        val = cfg['MisreadDensity']
+        cfg['MisreadDensity'] = {'default': val}
+    elif isinstance(cfg['MisreadDensity'], dict):
+        if 'default' not in cfg['MisreadDensity']:
+            raise ValueError("When MisreadDensity is a dictionary, it must contain a 'default' key")
+    else:
+        raise ValueError("MisreadDensity must be either a number or a dictionary with a 'default' key")
+
+    return spots, coo, cfg
+
+
+def _validate_coo(coo):
+    if isinstance(coo, coo_matrix):
+        coo = [coo]
+    if isinstance(coo, list):
+        assert np.all([isinstance(d, coo_matrix) for d in coo]), (
+            'The segmentation masks should be passed-in as a coo_matrix')
+
+        # check all planes have the same shape
+        shapes = [d.shape for d in coo]
+        assert shapes.count(shapes[0]) == len(shapes), 'Not all elements in coo have the same shape'
+    else:
+        raise ValueError('The segmentation masks should be passed-in as a coo_matrix')
+    return coo
+
+
+def _validate_spots(spots, coo, sc, cfg):
+    assert (isinstance(spots, pd.DataFrame)), 'Spots should be provided as a dataframe'
+
+    # check the spots
+    if not cfg['is3D']:
+        assert {'gene_name', 'x', 'y'}.issubset(spots.columns), ("Spots should be passed-in to the fit() "
+                                                            "method as a dataframe with columns ['gene_name', 'x', 'y']")
+    else:
+        assert set(spots.columns) == {'gene_name', 'x', 'y', 'z_plane'}, ("Spots should be passed-in to the fit() method "
+                                                                     "as a dataframe with columns 'gene_name', 'x',  'y', "
+                                                                     "'z_plane'")
+
+    if len(coo) > 1 and set(spots.columns) == {'gene_name', 'x', 'y'}:
+        raise ValueError("Label image is 3D, the spots are 2D")
+
+    # if you have feed a 2d spot, append a flat z dimension so that everything in the code from now on will
+    # be treated as 3d
+    if isinstance(spots, pd.DataFrame) and set(spots.columns) == {'gene_name', 'x', 'y'}:
+        spots = spots.assign(z_plane=0)
+
+    return spots
+
+
+def _validate_cfg(cfg, coo):
+    # The label image will determine whether dataset is 3d or 2d
+    cfg['is3D'] = False if len(coo) == 1 else True
+
+    # override the InsideCellBonus setting and set it to false if the dataset is 3D
+    # maybe drop a logger warning msg here too!
+    if cfg['is3D']:
+        cfg['InsideCellBonus'] = False
+
+    # if InsideCellBonus is True, set it to a numerical value
+    if cfg['InsideCellBonus'] is True:
+        """
+        This is not good enough! The default value for InsideCellBonus is now kept in two places, config.py and 
+        here. What happens if I change the config.py and I set InsideCellBonus = 3 for example? 
+        The line below will stll set it 2 which is not the default anymore! 
+        """
+        d = 2
+        cfg['InsideCellBonus'] = d
+        utils_logger.warning('InsideCellBonus was passed-in as True. Overriding with the default value of %d' % d)
+
+    if cfg['cell_type_prior'].lower() not in ['uniform'.lower(), 'weighted'.lower()]:
+        raise ValueError("'cel_type_prior' should be either 'uniform' or 'weighted' ")
+
+    # make sure the string is lowercase from now on
+    cfg['cell_type_prior'] = cfg['cell_type_prior'].lower()
+
+    if not isinstance(cfg['launch_viewer'], bool):
+        assert cfg['launch_viewer'].lower() == '2d', "'launch_viewer' should be True, False or '2d' "
+        cfg['launch_viewer'] = cfg['launch_viewer'].lower()
+
+    return cfg
+
+
+def clean_spots(spots, sc):
+    # remove genes that cannot been found in the single cell data
+    if (sc is not None) and (not set(spots.gene_name).issubset(sc.index)):
+        spots = purge_spots(spots, sc)
+    return spots
+
+
+def pre_launch(cellData, geneData, coo, scRNAseq, cfg):
+    '''
+    Does some housekeeping, pre-flight control checking before the
+    viewer is triggered
+
+    Returns the destination folder that keeps the viewer code and
+    will be served to launch the website
+    '''
+    cfg['is3D'] = False if cfg['launch_viewer'] == '2d' else cfg['launch_viewer']
+    [n, h, w] = get_img_shape(coo)
+    dst = get_out_dir(cfg['output_path'])
+    pciSeq_dir = copy_viewer_code(cfg, dst)
+    make_config(dst, pciSeq_dir, (cellData, scRNAseq, h, w))
+    if cfg['is3D']:
+        # ***************************************
+        # OK FOR NOW BUT THIS IS A TEMP FIX ONLY
+        # THESE LINES MUST BE REMOVED
+        cellData.X = cellData.X - w / 2
+        cellData.Y = cellData.Y - h / 2
+        cellData.Z = cellData.Z - n / 2
+
+        geneData.x = geneData.x - w / 2
+        geneData.y = geneData.y - h / 2
+        geneData.z = geneData.z - n / 2
+        # ***************************************
+
+        build_pointcloud(geneData, pciSeq_dir, dst)
+        cellData_rgb(cellData, pciSeq_dir, dst)
+        cell_gene_counts(geneData, dst)
+    return dst
+
+
+def make_config(target_dir, source_dir, data):
+    '''
+    makes the three javascript configuration files that drive the viewer.
+    They are getting saved into the dst folder. By default this is the tmp directory
+    '''
+
+    cellData = data[0]
+    scRNAseq = data[1]
+    img_shape = data[2:]
+    # 1. make the main configuration file
+    make_config_js(target_dir, img_shape)
+
+    # 2. make the file for the class colors
+    if scRNAseq is None:
+        label_list = [d[:] for d in cellData.ClassName.values]
+        labels = [item for sublist in label_list for item in sublist]
+        labels = sorted(set(labels))
+
+        make_classConfig_nsc_js(labels, target_dir)
+    else:
+        make_classConfig_js(source_dir, target_dir)
+
+    # 3. make the file for the glyph colors
+    gene_panel = scRNAseq.index.values
+    make_glyphConfig_js(gene_panel, source_dir, target_dir)
+
 
