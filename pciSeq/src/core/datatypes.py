fix: scaling down the mean gean reads from single cell data by the 'gene_inefficiency' parameter from the settings in the config file and then initialising gene inefficiency as a Gamma with a mean of 1.0

pciSeq/src/core/datatypes.py
@@ -846,10 +846,16 @@ class SingleCell(object):
         expr['Zero'] = np.zeros([expr.shape[0], 1])
         me = expr.rename_axis('gene_name').rename_axis("class_name", axis="columns")
 
-        # add the regularization parameter
-        me = me + self.config['SpotReg']
+        # multiply by the starting value of the gene inefficiency. That will scale down all the
+        # gene reads coming from the single cell data. (I don't quite like that tbh...why not leave
+        # single cell data as is and just initialise a gene inefficiency from a Gamma(a, b) where
+        # a = 1 and b = 1/self.config['Inefficiency']). Now we are mixing things up. If I want to
+        # get the expected gene inefficiency I will need to multiply the posterior by config['Inefficiency'].
+        # Similarly, for the mean gene expressions.
+        # I now have to look at two places if I want to fetch the gene inefficiency
+        me = me * self.config['Inefficiency'] + self.config['SpotReg']
 
-        # log mean expression
+        # log mean expression. Add the SpotRegularisation parameter to avoid zeros being fed into the log
         lme = np.log(me)
         return me, lme
 

pciSeq/src/core/main.py
@@ -161,7 +161,8 @@ class VarBayes:
     def initialise_state(self) -> None:
         self.cellTypes.ini_prior()
         self.cells.classProb = np.tile(self.cellTypes.prior, (self.nC, 1))
-        self.genes.init_eta(1, 1 / self.config['Inefficiency'])
+        self.genes.init_eta(1, 1)  # Initialise gene inefficiency as a Gamma(1, 1). Note that the single cell
+                                         # data have already been downscaled
         self.spots.parent_cell_id = self.spots.cells_nearby(self.cells)[0]
         self.spots.parent_cell_prob = self.spots.ini_cellProb(self.spots.parent_cell_id, self.config)
         self.cells._ini_gene_counts = np.bincount(self.spots.data.label.values, minlength=self.nC)
@@ -239,7 +240,7 @@ class VarBayes:
         gene_df = None
         max_iter = self.config['max_iter']
 
-        self.initialise_state()
+        # self.initialise_state()
         try:
             for i in range(max_iter):
                 self.iter_num = i
@@ -507,7 +508,7 @@ class VarBayes:
                                        gamma_bar[:, :, :-1])
         background_counts = self.cells.background_counts
         alpha = self.config['rGene'] + self.spots.counts_per_gene - background_counts - zero_class_counts
-        beta = self.config['rGene'] / self.config['Inefficiency'] + class_total_counts
+        beta = self.config['rGene'] + class_total_counts
 
         # Finally, update gene_gamma
         self.genes.calc_eta(alpha, beta)