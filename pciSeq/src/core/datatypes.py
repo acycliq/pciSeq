fix: Proper handling of the calculation for the posterior mean of gene inefficiency.

pciSeq/src/core/datatypes.py
@@ -342,7 +342,7 @@ class Genes(object):
         nG (int): Number of genes.
     """
 
-    def __init__(self, spots):
+    def __init__(self, spots, config: Dict):
         """
         Initializes the Genes object with spot data.
 
@@ -353,6 +353,7 @@ class Genes(object):
         self._eta_bar = None
         self._logeta_bar = None
         self.nG = len(self.gene_panel)
+        self.config = config
 
     @property
     def eta_bar(self):
@@ -361,8 +362,18 @@ class Genes(object):
 
     @property
     def logeta_bar(self):
-        """Returns the log eta bar values for genes."""
+        """Returns the log eta bar for genes (estimated mean of the posterior)."""
         return self._logeta_bar
+
+    @property
+    def inefficiency(self):
+        """
+        Returns the gene inefficiency
+        The actual gene inefficiency is the estimated mean of the posterior (eta_bar)
+        multiplied by the inefficiency (user-defined) value that was passed in the algo
+        via the configuration file
+        """
+        return self.eta_bar * self.config['Inefficiency']
 
     def init_eta(self, a, b):
         """
@@ -400,6 +411,47 @@ class Genes(object):
             np.array: Digamma values.
         """
         return scipy.special.psi(a) - np.log(b)
+
+    def get_inefficiency(self, gene=None):
+        """
+        Retrieve the inefficiency values for one or more genes.
+
+        This is a convenience method that returns inefficiency values from the gene panel.
+        it returns a  DataFrame with the genes and the corresponding inefficiencies.
+
+        Parameters:
+        ----------
+        gene : str, list of str, or None (default: None)
+            - If None, returns inefficiency values for all genes.
+            - If a string, returns inefficiency for the specified gene as a DataFrame.
+            - If a list of strings, returns inefficiency values for the specified genes.
+
+        Returns:
+        -------
+        pandas.DataFrame
+            A DataFrame with genes as the index and inefficiency values as the column.
+            Missing genes will be included with NaN values.
+
+        Raises:
+        ------
+        TypeError
+            If `gene` is not a string, list of strings, or None.
+        """
+        df = pd.DataFrame(
+            {'inefficiency': self.inefficiency},
+            index=self.gene_panel
+        )
+
+        if gene is None:
+            return df  # Return full DataFrame
+
+        if isinstance(gene, str):
+            return df.loc[[gene]] if gene in df.index else pd.DataFrame(columns=df.columns, index=[gene])
+
+        if isinstance(gene, list):
+            return df.reindex(gene)  # Handles missing genes gracefully (NaN for missing ones)
+
+        raise TypeError("Expected gene to be a string, list, or None.")
 
 
 # ----------------------------------------Class: Spots--------------------------------------------------- #
@@ -747,7 +799,7 @@ class SingleCell(object):
         # try to estimate them
         # self.raw_data = self._raw_data(scdata, genes)
         self.config = config
-        self._mean_expression, self._log_mean_expression = self._setup(scdata, genes, self.config)
+        self._mean_expression_adj, self._log_mean_expression_adj = self._setup(scdata, genes, self.config)
 
     def _setup(self, scdata: pd.DataFrame, genes: np.ndarray, config: Dict) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
@@ -772,45 +824,51 @@ class SingleCell(object):
             self.isMissing = False
 
         self.raw_data = expr
-        me, lme = self._helper(expr.copy())
 
-        assert me.columns[-1] == 'Zero', "Last column should be the Zero class"
-        assert lme.columns[-1] == 'Zero', "Last column should be the Zero class"
-        return me.astype(np.float32), lme.astype(np.float32)
+        # get the mean (and log-mean) expression data by cell type.
+        # Figures have been scaled by the gene inefficiency
+        me_adj, lme_adj = self._helper(expr.copy())
+
+        assert me_adj.columns[-1] == 'Zero', "Last column should be the Zero class"
+        assert lme_adj.columns[-1] == 'Zero', "Last column should be the Zero class"
+        return me_adj.astype(np.float32), lme_adj.astype(np.float32)
 
     # -------- PROPERTIES -------- #
     @property
-    def mean_expression(self):
-        """Returns the mean expression levels."""
-        assert self._mean_expression.columns[-1] == 'Zero', "Last column should be the Zero class"
-        return self._mean_expression
+    def mean_expression_adj(self):
+        """Returns the mean expression levels adjusted by the initial gene inefficiency."""
+        assert self._mean_expression_adj.columns[-1] == 'Zero', "Last column should be the Zero class"
+        return self._mean_expression_adj
 
     @property
     def log_mean_expression(self):
-        """Returns the log mean expression levels."""
-        assert self._log_mean_expression.columns[-1] == 'Zero', "Last column should be the Zero class"
-        return self._log_mean_expression
+        """Returns the log mean expression levels adjusted by the initial gene inefficiency."""
+        assert self._log_mean_expression_adj.columns[-1] == 'Zero', "Last column should be the Zero class"
+        return self._log_mean_expression_adj
+
+    @property
+    def mean_expression(self):
+        """Returns the mean gene counts per cell class"""
+        return (self.mean_expression_adj - self.config['SpotReg']) / self.config['Inefficiency']
 
     @property
     def genes(self):
-        """Returns the gene names."""
-        return self.mean_expression.index.values
+        """
+        Returns the gene names.
+        WARNING: you can get the gene names from the Genes object and its gene_panel property.
+        They should be the same but is there any value having two places for the same thing?
+        """
+        return self.mean_expression_adj.index.values
 
     @property
     def classes(self):
         """Returns the class names."""
-        return self.mean_expression.columns.values
+        return self.mean_expression_adj.columns.values
 
-    ## Helper functions ##
+    # Helper functions #
     def _set_axes(self, df):
         """
         Sets the axes labels for a DataFrame.
-
-        Parameters:
-            df (pd.DataFrame): DataFrame to set axes for.
-
-        Returns:
-            pd.DataFrame: DataFrame with set axes.
         """
         df = df.rename_axis("class_name", axis="columns").rename_axis('gene_name')
         return df
@@ -818,12 +876,6 @@ class SingleCell(object):
     def _remove_zero_cols(self, df):
         """
         Removes columns with all zero values from a DataFrame.
-
-        Parameters:
-            df (pd.DataFrame): DataFrame to remove zero columns from.
-
-        Returns:
-            pd.DataFrame: DataFrame with zero columns removed.
         """
         out = df.loc[:, (df != 0).any(axis=0)]
         return out
@@ -846,8 +898,27 @@ class SingleCell(object):
         expr['Zero'] = np.zeros([expr.shape[0], 1])
         me = expr.rename_axis('gene_name').rename_axis("class_name", axis="columns")
 
-        # add the regularization parameter
-        me = me + self.config['SpotReg']
+        # Apply the inefficiency and add the regularization parameter
+        # Note:     04-Feb-2025: The inefficiency is modelled as a Gamma distribution.
+        #           If we denote inefficiency by X then we re-express it
+        #           as X = c * Y, where Y is Gamma with some parameter rGene
+        #           which is set in the config file. Note that the constant
+        #           is also set in the config file too under the name Inefficiency.
+        #           Hence:
+        #               Y ~ Gamma(rGene, rGene)
+        #               X = c * Y => X ~  Gamma(rGene, rGene/c)
+        #           Here the constant c is 'hijacked' and applied directly on
+        #           single cell data to scale them down.
+        #           In the rest of the code, is assumed to be from Gamma(rGene, rGene)
+        #           and this is reflected when the prior for the inefficiency is initialised
+        #           and in the step inside the Variational Bayes that calcs the posterior
+        #           distribution for the inefficiency (eta_upd)
+        #           This also means that the actual gene inefficiency, the one that
+        #           the end-user cares about is:
+        #               the posterior calculated from the eta_upd step
+        #                               times
+        #                       self.config['Inefficiency']
+        me = me * self.config['Inefficiency'] + self.config['SpotReg']
 
         # log mean expression
         lme = np.log(me)

pciSeq/src/core/main.py
@@ -145,7 +145,7 @@ class VarBayes:
         """Set up the core data components needed for the algorithm."""
         self.cells = Cells(cells_df, self.config)
         self.spots = Spots(spots_df, self.config)
-        self.genes = Genes(self.spots)
+        self.genes = Genes(self.spots, self.config)
         self.single_cell = SingleCell(scRNAseq, self.genes.gene_panel, self.config)
         self.cellTypes = CellType(self.single_cell, self.config)
         self.cells.class_names = self.single_cell.classes
@@ -159,9 +159,14 @@ class VarBayes:
         self.nN = self.config['nNeighbors'] + 1  # neighbors + background
 
     def initialise_state(self) -> None:
+        """Initialises the starting state of the objects
+        4-Feb-2025: Note that inefficiency (denoted by eta) follows a Gamma(rGene, rGene).
+        Keep in mind also that there is also the config['Inefficiency'] parameter that has
+        been applied directly to the expression data from scRNAseq
+        """
         self.cellTypes.ini_prior()
         self.cells.classProb = np.tile(self.cellTypes.prior, (self.nC, 1))
-        self.genes.init_eta(1, 1 / self.config['Inefficiency'])
+        self.genes.init_eta(self.config['rGene'], self.config['rGene'])
         self.spots.parent_cell_id = self.spots.cells_nearby(self.cells)[0]
         self.spots.parent_cell_prob = self.spots.ini_cellProb(self.spots.parent_cell_id, self.config)
         self.cells._ini_gene_counts = np.bincount(self.spots.data.label.values, minlength=self.nC)
@@ -255,9 +260,23 @@ class VarBayes:
 
                 # 4. assign cells to cell types
                 self.cell_to_cellType()
+                out = pd.DataFrame({'class_name': self.cells.class_names,
+                                    'prob': self.cells.classProb[29023]
+                                    }).sort_values(by='prob', ascending=False)
+                print(out.head(5))
 
                 # 5. assign spots to cells
                 self.spots_to_cell()
+                mask = self.spots.parent_cell_id == 29023
+                prob = self.spots.parent_cell_prob[mask]
+                out = self.spots.data[mask.sum(axis=1).astype(bool)]
+                out = out.assign(prob=prob)
+                out = (out.groupby("gene_name", as_index=False)["prob"]
+                       .sum()
+                       .sort_values(by='prob', ascending=False))
+                print(out.head(8))
+
+
 
                 # 6. update gene efficiency
                 self.eta_upd()
@@ -365,7 +384,7 @@ class VarBayes:
         cfg = self.config
 
         self._scaled_exp = delayed(utils.scaled_exp(cells.ini_cell_props['area_factor'],
-                                                    self.single_cell.mean_expression.values,
+                                                    self.single_cell.mean_expression_adj.values,
                                                     self.genes.eta_bar))
 
         beta = self.scaled_exp.compute() + cfg['rSpot']
@@ -488,7 +507,7 @@ class VarBayes:
             'The sum of the background spots and the total gene counts should be equal to the number of spots'
 
         classProb = self.cells.classProb
-        mu = self.single_cell.mean_expression
+        mu = self.single_cell.mean_expression_adj
         area_factor = self.cells.ini_cell_props['area_factor']
         gamma_bar = self.spots.gamma_bar.compute()
 
@@ -506,7 +525,7 @@ class VarBayes:
                                        gamma_bar[:, :, :-1])
         background_counts = self.cells.background_counts
         alpha = self.config['rGene'] + self.spots.counts_per_gene - background_counts - zero_class_counts
-        beta = self.config['rGene'] / self.config['Inefficiency'] + class_total_counts
+        beta = self.config['rGene'] + class_total_counts
 
         # Finally, update gene_gamma
         self.genes.calc_eta(alpha, beta)