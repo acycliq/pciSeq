d6e9e03320a457cebb26cb983d779907cae7c5ff
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2025-02-25 11:42:54 +0000

    feature: add shapely and alphashape as a dependency in the setup file

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ def get_static_files(root):
 install_deps = ['numpy_groupies', 'pandas', 'dask', 'scipy', 'streamlit', 'altair',
                 'scikit-image', 'scikit-learn', 'tqdm', 'flask', 'fastremap',
                 'numexpr', 'diplib', 'pyvips', 'natsort', 'redis',
-                'matplotlib', 'laspy', 'tomlkit', 'colorlog']
+                'matplotlib', 'laspy', 'tomlkit', 'colorlog',
+                'shapely', 'alphashape']
 
 version = None
 with open(os.path.join('pciSeq', '_version.py'), 'r') as fid:
