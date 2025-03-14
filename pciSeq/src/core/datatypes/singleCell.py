fix: scaled expressions formula

pciSeq/src/core/datatypes/singleCell.py
@@ -91,7 +91,7 @@ class SingleCell(object):
     @property
     def mean_expression(self):
         """Returns the mean gene counts per cell class"""
-        return (self.mean_expression_adj - self.config['SpotReg']) / self.config['Inefficiency']
+        return self.mean_expression_adj / self.config['Inefficiency']
 
     @property
     def genes(self):
@@ -160,10 +160,10 @@ class SingleCell(object):
         #               the posterior calculated from the eta_upd step
         #                               times
         #                       self.config['Inefficiency']
-        me = me * self.config['Inefficiency'] + self.config['SpotReg']
+        me = me * self.config['Inefficiency']
 
         # log mean expression
-        lme = np.log(me)
+        lme = np.log(me + self.config['SpotReg'])
         return me, lme
 
     def _gene_expressions(self, fitted, scale):

pciSeq/src/core/main.py
@@ -378,8 +378,7 @@ class VarBayes:
         cfg = self.config
 
         self._scaled_exp = delayed(utils.scaled_exp(cells.ini_cell_props['area_factor'],
-                                                    self.single_cell.mean_expression_adj.values,
-                                                    self.genes.eta_bar))
+                                                    self.single_cell.mean_expression_adj.values))
 
         beta = self.scaled_exp.compute() + cfg['rSpot']
         rho = cfg['rSpot'] + cells.geneCount
@@ -402,7 +401,8 @@ class VarBayes:
             3. Softmax normalization for final probabilities
         """
 
-        ScaledExp = self.scaled_exp.compute()
+        ScaledExp = np.einsum('cgk,g->cgk', self.scaled_exp.compute(), self.genes.eta_bar) + self.config['SpotReg']
+        # ScaledExp = self.scaled_exp.compute() * self.genes.eta_bar + self.config['SpotReg']
         pNegBin = ScaledExp / (self.config['rSpot'] + ScaledExp)
         cgc = self.cells.geneCount
         contr = utils.negative_binomial_loglikelihood(cgc, self.config['rSpot'], pNegBin)
@@ -529,7 +529,7 @@ class VarBayes:
         self.spots.parent_cell_prob = softmax(wSpotCell, axis=1)
 
         # Update gene counts
-        self.geneCount_upd()
+        # self.geneCount_upd()
 
     # -------------------------------------------------------------------- #
     def eta_upd(self) -> None:
@@ -546,12 +546,12 @@ class VarBayes:
         Note:
             The zero-expressing cell class is excluded from the computation.
         """
-        grand_total = self.cells.background_counts.sum() + self.cells.total_counts.sum()
-        assert round(grand_total) == self.spots.data.shape[0], \
-            'The sum of the background spots and the total gene counts should be equal to the number of spots'
+        # grand_total = self.cells.background_counts.sum() + self.cells.total_counts.sum()
+        # assert round(grand_total) == self.spots.data.shape[0], \
+        #     'The sum of the background spots and the total gene counts should be equal to the number of spots'
 
         classProb = self.cells.classProb
-        mu = self.single_cell.mean_expression_adj
+        mu = self.single_cell.mean_expression_adj + self.config['SpotReg']
         area_factor = self.cells.ini_cell_props['area_factor']
         gamma_bar = self.spots.gamma_bar.compute()
 

pciSeq/src/core/utils/ops_utils.py
@@ -159,24 +159,18 @@ def has_converged(
 
 
 def scaled_exp(cell_area_factor: np.ndarray,
-               sc_mean_expressions: np.ndarray,
-               inefficiency: np.ndarray) -> np.ndarray:
+               sc_mean_expressions: np.ndarray) -> np.ndarray:
     """Calculate scaled expression values.
 
     Args:
         cell_area_factor: Cell area scaling factors
         sc_mean_expressions: Single cell mean expression values
-        inefficiency: Inefficiency factors
 
     Returns:
         Scaled expression array
     """
-    if np.all(cell_area_factor == 1):
-        subscripts = 'gk, g -> gk'
-        operands = [sc_mean_expressions, inefficiency]
-    else:
-        subscripts = 'c, gk, g -> cgk'
-        operands = [cell_area_factor, sc_mean_expressions, inefficiency]
+    subscripts = 'c,gk->cgk'
+    operands = [cell_area_factor, sc_mean_expressions]
 
     return oe.contract(subscripts, *operands, optimize='optimal')
 