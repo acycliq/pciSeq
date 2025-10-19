5ba18ea43250ba2d786cb10576658904e40a9d7b
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2025-10-19 10:35:21 +0100

    Update check_cell column headers for clarity

--- pciSeq/src/core/utils/ops_utils.py
+++ pciSeq/src/core/utils/ops_utils.py
@@ -351,11 +351,11 @@ def check_cell(obj, label, user_class, top_n=10, show_plot=True):
     )
 
     # Add the MultiIndex header
-    new_columns = pd.MultiIndex.from_tuples(
-        [('pciSeq class avg counts', pciSeq_class),
-         ('pciSeq class avg counts', user_class),
-         (f'Cell: {label}', 'Gene Counts')]  # Leave the third column header as is
-    )
+    new_columns = pd.MultiIndex.from_tuples([
+        (f'Cells typed as {pciSeq_class}', 'mean counts'),
+        (f'Cells typed as {user_class}', 'mean counts'),
+        (f'This cell: ({label})', 'counts')
+    ])
     gene_expression_data.columns = new_columns
 
     if show_plot:
