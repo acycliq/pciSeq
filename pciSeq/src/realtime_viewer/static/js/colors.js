cb2094a50c0030458484f5d9f031ab9944db6500
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2025-11-10 15:44:33 +0000

    Use d3.js for hslToRgb conversion

--- pciSeq/src/realtime_viewer/static/js/colors.js
+++ pciSeq/src/realtime_viewer/static/js/colors.js
@@ -22,17 +22,13 @@
         }
     }
 
-    // HSL to RGB conversion
+    // HSL to RGB conversion (use d3 for clarity and reliability)
     function hslToRgb(h, s, l) {
-        s /= 100;
-        l /= 100;
-        const k = n => (n + h / 30) % 12;
-        const a = s * Math.min(l, 1 - l);
-        const f = n => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+        const rgb = d3.hsl(h, s / 100, l / 100).rgb();
         return [
-            Math.round(255 * f(0)),
-            Math.round(255 * f(8)),
-            Math.round(255 * f(4))
+            Math.round(rgb.r),
+            Math.round(rgb.g),
+            Math.round(rgb.b)
         ];
     }
 
