ea3a890f96175c40485b343f4b7d2d7316b0b1f7
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2026-03-18 11:38:07 +0000

    remove some debugging CSVs saving

--- pciSeq/src/core/main.py
+++ pciSeq/src/core/main.py
@@ -444,20 +444,20 @@ class VarBayes:
         wCellClass = contr + self.cellTypes.log_prior + 1.5 * mrf
         pCellClass = softmax(wCellClass, axis=1)
 
-        # save the data to a tmp dir
-        if (self.iter_num < 10) or (self.iter_num > 70):
-            from pathlib import Path
-            out_dir = Path("/tmp/pciSeq/data/flatfiles") / f"iter_{self.iter_num}"
-            out_dir.mkdir(parents=True, exist_ok=True)
-
-            for i, d in enumerate(df_list):
-                d.to_csv(out_dir / f"contr_{i}.csv", index=False)
-
-            pd.DataFrame(mrf, columns=self.cells.class_names).to_csv(out_dir / "mrf.csv")
-
-            # if log_prior is (K,) make it a single row; if it's already (1,K) or (N,K) this also works if you adjust
-            pd.DataFrame([self.cellTypes.log_prior], columns=self.cells.class_names).to_csv(out_dir / "log_prior.csv")
-            main_logger.info(f"[iter {self.iter_num}] Saving debug CSVs to: {out_dir}")
+        # # save the data to a tmp dir
+        # if (self.iter_num < 10) or (self.iter_num > 70):
+        #     from pathlib import Path
+        #     out_dir = Path("/tmp/pciSeq/data/flatfiles") / f"iter_{self.iter_num}"
+        #     out_dir.mkdir(parents=True, exist_ok=True)
+        #
+        #     for i, d in enumerate(df_list):
+        #         d.to_csv(out_dir / f"contr_{i}.csv", index=False)
+        #
+        #     pd.DataFrame(mrf, columns=self.cells.class_names).to_csv(out_dir / "mrf.csv")
+        #
+        #     # if log_prior is (K,) make it a single row; if it's already (1,K) or (N,K) this also works if you adjust
+        #     pd.DataFrame([self.cellTypes.log_prior], columns=self.cells.class_names).to_csv(out_dir / "log_prior.csv")
+        #     logger.info(f"[iter {self.iter_num}] Saving debug CSVs to: {out_dir}")
 
         self.cells.classProb = pCellClass
 
